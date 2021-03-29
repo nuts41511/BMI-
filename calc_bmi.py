6aac8a3b5e1bebdcc5c807d32292be32787f18ab
nishouji rinpei <r.nishouji.sys20@morijyobi.ac.jp>
2021-03-29 12:15:43 +0900

    BMItukuttayo

---
+++ calc_bmi.py
@@ -0,0 +1,3 @@
+def get_area(sintyou,taijuu):
+    area = taijuu / (sintyou * sintyou)
+    return area
---
+++ main.py
@@ -0,0 +1,7 @@
+import calc_bmi
+
+print("BMI計算アプリ")
+s = int(input("身長:"))
+t = int(input("体重:"))
+
+print(f"BMI:{calc_bmi.get_area(s,t)}")
