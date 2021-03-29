d053c1059f2dd1eb380e39415890a50206e7c3e3
nishouji rinpei <r.nishouji.sys20@morijyobi.ac.jp>
2021-03-29 12:24:28 +0900

    なおした！

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ s = int(input("身長:"))
 t = int(input("体重:"))
 
 print(f"BMI:{calc_bmi.get_area(s,t)}")
+
+
+print("2回目")
+s = int(input("スン超"))
+t = int(input("体重"))
+
+print(f"BMI:{calc_bmi.get_area(s,t)}")
+
+
